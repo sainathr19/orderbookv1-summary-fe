f98de4502622cac9b8d1803145755711167d0a77
Sainath Reddy <gsnr1925@gmail.com>
2025-01-28 12:53:50 +0530

    Update formatDate function to take UTC date

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -19,9 +19,9 @@ export function formatAmount(rawAmount: string, decimals: number): number {
 
 export const formatDate = (rawDate: string): string => {
   const date = new Date(rawDate);
-  const day = String(date.getDate()).padStart(2, '0');
-  const month = String(date.getMonth() + 1).padStart(2, '0');
-  const year = String(date.getFullYear());
+  const day = String(date.getUTCDate()).padStart(2, '0');
+  const month = String(date.getUTCMonth() + 1).padStart(2, '0'); 
+  const year = String(date.getUTCFullYear());
   return `${day}/${month}/${year}`;
 };
 
